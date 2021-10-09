b919b41546a75a5342c4913e3f814d6ed29c4fd6
seastar <2229029156@qq.com>
2021-10-09 22:14:25 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except ImportError:
 setup(
     name='CNN4IE',
     author='ShiYan',
-    version='0.1.2',
+    version='0.1.3',
     license='Apache 2.0',
     description='Chinese Information Extraction',
     long_description=readme,
